b5cbff5b7e41f9c19714957531f1940040860671
esteok <114547516+esteok@users.noreply.github.com>
2023-03-13 15:56:33 -0400

    Add files via upload

---
+++ project2/js/index.js
@@ -0,0 +1,3 @@
+document.write("<div style='text-align:center;'>");
+document.write("<a href='default.html'>Home</a> | <a href='cloud.html'>Cloud Computing</a> | <a href='ai.html'>Artificial Intelligence</a> | <a href='careers.html'>Careers In I.T</a");
+document.write("</div>")
